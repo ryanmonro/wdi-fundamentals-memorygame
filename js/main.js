ddd074e6d3e64e6f65d2783e7f1ca124cd6be158
ryanmonro <ryan@ryanmonro.info>
2017-08-22 14:41:29 +1000

    created game logic

---
+++ js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+var cardOne = "queen"
+	cardTwo = "king"
+	cardThree = "queen"
+	cardFour = "king";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
